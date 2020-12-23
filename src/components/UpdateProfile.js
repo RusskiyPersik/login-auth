57de8c11e4b17378ca5b35ef23db1e8a4de0f395
Jeff Olupo <olupo.jeff@gmail.com>
2020-12-23 16:57:52 -0600

    completed functionality of setting up firebase to my login app

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -7,29 +7,37 @@ export default function UpdateProfile() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const { currentUser } = useAuth()
+    const { currentUser, updatePassword, updateEmail } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const history = useHistory()
 
-    async function handleSumbmit(e){
-        // e.preventDefault()
+    function handleSumbmit(e){
+        e.preventDefault()
 
-        // if(passwordRef.current.value !==
-        // passwordConfirmRef.current.value){
-        //     return setError('Passwords do not match')
-        // }
+        if(passwordRef.current.value !==
+        passwordConfirmRef.current.value){
+            return setError('Passwords do not match')
+        }
 
-        // try {
-        //     setError('')
-        //     setLoading(true)
-        //     await signup(emailRef.current.value, passwordRef.current.value)
-        //     history.push("/")
-        // } catch {
-        //     setError('Failed to create an account')
-        // }
+        const promises = []
+        setLoading(true)
+        setError("")
 
-        // setLoading(false)
+        if (emailRef.current.value !== currentUser.email){
+            promises.push(updateEmail(emailRef.current.value))
+        }
+        if (passwordRef.current.value){
+            promises.push(updatePassword(passwordRef.current.value))
+        }
+
+        Promise.all(promises).then(() => {
+            history.push('/')
+        }).catch(() => {
+            setError('Failed to update account')
+        }).finally(() => {
+            setLoading(false)
+        })
         
     }
 
@@ -50,7 +58,6 @@ export default function UpdateProfile() {
                             <Form.Control 
                                 type="password" 
                                 ref={passwordRef} 
-                                required 
                                 placeholder='Leave blank to keep the same'
                             />
                         </Form.Group>
@@ -58,8 +65,7 @@ export default function UpdateProfile() {
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control 
                                 type="password" 
-                                ref={passwordRef} 
-                                required 
+                                ref={passwordConfirmRef}  
                                 placeholder='Leave blank to keep the same'
                             />
                         </Form.Group>
